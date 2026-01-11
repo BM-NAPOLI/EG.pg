01c50500c52dc8289c156799310f9057a8526c6e
ahmadihya9860 <Ahmadihya9860@gmail.com>
2026-01-11 19:16:56 +0100

    I added the Task class

--- task.py
+++ task.py
@@ -1,1 +1,13 @@
-print()
+
+
+class Task:
+    # had dala hiya li kat7km f l9iyam li hkayakhwd l class fach hkatstad3ih 
+    def __init__(self,title,description,isDone=False):
+
+        # hna kan3rf ana dok l9iyam li dakhla li f dik __init__ kanrbtha b lkain li ghaytncha 
+        self.title = title
+        self.description = description
+        self.isDone = isDone
+    
+
+    
